f48dd705295eba8f6ab31c6ed4ccba45214032d0
Chitakov <chitakov2606nikita@gmail.com>
2022-06-17 18:31:05 +0300

    Deleted constructor Employee

--- Lab12_v2/Employee.cs
+++ Lab12_v2/Employee.cs
@@ -8,12 +8,6 @@ namespace Lab12_v2
 {
     class Employee
     {
-        //public Employee(int id, string name, string phonenumber)
-        //{
-        //    this.Id = id;
-        //    this.Name = name;
-        //    this.PhoneNumber = phonenumber;
-        //}
         public int Id { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
